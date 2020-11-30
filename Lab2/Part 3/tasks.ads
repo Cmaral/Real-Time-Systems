83d793b0c7ad0174fc540797f152eaea42fb5775
Claudia M. A <claumartal@gmail.com>
2020-11-30 13:21:49 +0100

    Update tasks.ads

--- Lab2/Part 3/tasks.ads
+++ Lab2/Part 3/tasks.ads
@@ -6,7 +6,7 @@ package Tasks is
 private
 
   --  Define periods and times  --  
-  Period_MotorControl : Time_Span := Milliseconds(250);
+  Period_MotorControl : Time_Span := Milliseconds(200);
   Period_LineFollowing : Time_Span := Milliseconds(100);
   Period_Distance : Time_Span := Milliseconds(100);
   Period_Display : Time_Span := Milliseconds(500); 
